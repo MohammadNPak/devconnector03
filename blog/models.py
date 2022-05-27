86fe5cc344fc1a221d4e36f7c099db8bb57cf54f
mohammadnpak <m.nozaripak@gmail.com>
2022-05-27 12:40:13 +0430

    s08-c

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+user =get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.title}|{self.body[:20]}"
@@ -19,6 +23,8 @@ class Comment(models.Model):
     body = models.TextField()
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
+    
 
     def __str__(self) -> str:
         return f"{self.post.title}|{self.body[:40]}"
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from .models import Post, Comment, SamplePost
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
@@ -8,22 +9,22 @@ from .forms import PostForm, CommentForm
 # Create your views here.
 
 
-
+@login_required(login_url='/accounts/login/')
 def post(request, id):
-    u = get_user_model().objects.first()
+    # u = get_user_model().objects.first()
     p = get_object_or_404(Post, id=id)
     if request.method == "POST":
         comment = CommentForm(request.POST)
         if comment.is_valid:
             comment_object = comment.save(commit=False)
             comment_object.post = p
+            comment_object.author = request.user
             comment_object.save()
 
     c = Comment.objects.filter(post=p).order_by('-date')
     form = CommentForm()
     return render(request, 'blog/post.html',
                   {'post': p,
-                   'u':u ,
                    'comments': c,
                    'form': form
                    })
@@ -33,6 +34,7 @@ def index(request):
     return render(request, 'blog/index.html', {})
 
 
+@login_required(login_url='/accounts/login/')
 def posts(request):
     if request.method == "POST":
         form = PostForm(request.POST)
